8b1289723ceb798782070ce2aa8048366d22ca1b
epalyanov <e.palyanov@tetra-soft.ru>
2025-07-22 18:24:37 +0300

    add error style

--- src/App.js
+++ src/App.js
@@ -1,98 +1,103 @@
-import { fetchData } from "./services/apiService.js";
+import {fetchData} from "./services/apiService.js";
 
 const LIMIT = 3;
 
 export class App {
-  constructor() {
-    this.source = "quotes";
-    this.theme = "light";
-    this.page = 0;
-    this.total = 0;
-    this.container = document.getElementById("card-container");
-    this.pageInfo = document.getElementById("page-info");
-    this.cardElements = [];
-    this.prevBtn = document.getElementById("prev");
-    this.nextBtn = document.getElementById("next");
-  }
-
-  init() {
-    document.getElementById("data-source").addEventListener("change", (e) => {
-      this.source = e.target.value;
-      this.page = 0;
-      this.loadData();
-    });
-
-    document
-      .getElementById("theme-selector")
-      .addEventListener("change", (e) => {
-        this.theme = e.target.value;
-        document.documentElement.setAttribute("data-bs-theme", this.theme);
-      });
-
-    this.prevBtn.addEventListener("click", () => {
-      if (this.page > 0) {
-        this.page--;
-        this.loadData();
-      }
-    });
-
-    this.nextBtn.addEventListener("click", () => {
-      if ((this.page + 1) * LIMIT < this.total) {
-        this.page++;
-        this.loadData();
-      }
-    });
-
-    this.loadData();
-  }
-
-  async loadData() {
-    this.renderCards(null);
-    const skip = this.page * LIMIT;
-
-    try {
-      const data = await fetchData(this.source, LIMIT, skip);
-      this.total = data.total;
-      this.renderCards(data.items);
-      this.updatePagination();
-    } catch (err) {
-      this.container.innerHTML = "Ошибка загрузки данных";
-    }
-  }
-
-  updatePagination() {
-    this.pageInfo.textContent = `Страница ${this.page + 1} из ${Math.ceil(
-      this.total / LIMIT
-    )}`;
-    this.prevBtn.disabled = this.page === 0;
-    this.nextBtn.disabled = (this.page + 1) * LIMIT >= this.total;
-  }
-
-  renderCards(items) {
-    const tag = this.source === "quotes" ? "card-quote" : "card-todo";
-
-    if (
-      this.cardElements.length !== LIMIT ||
-      this.cardElements[0]?.tagName.toLowerCase() !== tag
-    ) {
-      this.container.innerHTML = "";
-      this.cardElements = [];
-
-      for (let i = 0; i < LIMIT; i++) {
-        const el = document.createElement(tag);
-        this.cardElements.push(el);
-        this.container.appendChild(el);
-      }
+    constructor() {
+        this.source = "quotes";
+        this.theme = "light";
+        this.page = 0;
+        this.total = 0;
+        this.container = document.getElementById("card-container");
+        this.pageInfo = document.getElementById("page-info");
+        this.cardElements = [];
+        this.prevBtn = document.getElementById("prev");
+        this.nextBtn = document.getElementById("next");
     }
 
-    if (!items) {
-      this.cardElements.forEach((el) => el.showSkeleton());
-    } else {
-      this.cardElements.forEach((el, i) => {
-        if (items[i]) {
-          el.data = items[i];
+    init() {
+        document.getElementById("data-source").addEventListener("change", (e) => {
+            this.source = e.target.value;
+            this.page = 0;
+            this.loadData();
+        });
+
+        document
+            .getElementById("theme-selector")
+            .addEventListener("change", (e) => {
+                this.theme = e.target.value;
+                document.documentElement.setAttribute("data-bs-theme", this.theme);
+            });
+
+        this.prevBtn.addEventListener("click", () => {
+            if (this.page > 0) {
+                this.page--;
+                this.loadData();
+            }
+        });
+
+        this.nextBtn.addEventListener("click", () => {
+            if ((this.page + 1) * LIMIT < this.total) {
+                this.page++;
+                this.loadData();
+            }
+        });
+
+        this.loadData();
+    }
+
+    async loadData() {
+        this.renderCards(null);
+        const skip = this.page * LIMIT;
+
+        try {
+            const data = await fetchData(this.source, LIMIT, skip);
+            this.total = data.total;
+            this.renderCards(data.items);
+            this.updatePagination();
+        } catch (err) {
+            this.renderError();
         }
-      });
     }
-  }
+
+    updatePagination() {
+        this.pageInfo.textContent = `Страница ${this.page + 1} из ${Math.ceil(
+            this.total / LIMIT
+        )}`;
+        this.prevBtn.disabled = this.page === 0;
+        this.nextBtn.disabled = (this.page + 1) * LIMIT >= this.total;
+    }
+
+    renderError() {
+        this.cardElements.length = 0;
+        this.container.innerHTML = `<div class="alert alert-danger" role="alert">Ошибка загрузки данных!</div>`;
+    }
+
+    renderCards(items) {
+        const tag = this.source === "quotes" ? "card-quote" : "card-todo";
+
+        if (
+            this.cardElements.length !== LIMIT ||
+            this.cardElements[0]?.tagName.toLowerCase() !== tag
+        ) {
+            this.container.innerHTML = "";
+            this.cardElements = [];
+
+            for (let i = 0; i < LIMIT; i++) {
+                const el = document.createElement(tag);
+                this.cardElements.push(el);
+                this.container.appendChild(el);
+            }
+        }
+
+        if (!items) {
+            this.cardElements.forEach((el) => el.showSkeleton());
+        } else {
+            this.cardElements.forEach((el, i) => {
+                if (items[i]) {
+                    el.data = items[i];
+                }
+            });
+        }
+    }
 }
--- src/components/CardQuote.js
+++ src/components/CardQuote.js
@@ -1,8 +1,8 @@
-import { CardElement } from "./CardBaseClass.js";
+import {CardElement} from "./CardBaseClass.js";
 
 export class CardQuote extends CardElement {
-  set data(value) {
-    this.innerHTML = `
+    set data(value) {
+        this.innerHTML = `
       <div class="card">
           <div class="card-body">
               <h5 class="card-title">${value.quote}</h5>
@@ -10,7 +10,7 @@ export class CardQuote extends CardElement {
           </div>
       </div>
     `;
-  }
+    }
 }
 
 customElements.define("card-quote", CardQuote);
